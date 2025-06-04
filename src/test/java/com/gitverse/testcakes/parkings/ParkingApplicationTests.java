e883033f06f38e5b1958657db54db92fb822be2b
tennyros <vvkamalov_obs@mail.ru>
2025-06-04 20:09:39 +0500

    feat: initial project structure setup with pom.xml, application.yml files, main app and test java classes

---
+++ src/test/java/com/gitverse/testcakes/parkings/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitverse.testcakes.parkings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
